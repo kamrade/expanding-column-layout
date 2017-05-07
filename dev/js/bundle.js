01bd0dcc6624a2fca0fc2fd986c10ae93001ab6f
Denis Michailov <kamrad.reg@gmail.com>
2017-05-07 11:44:44 +1000

    process...

--- dev/js/bundle.js
+++ dev/js/bundle.js
@@ -10268,30 +10268,38 @@ var Expand = (function() {
 
     var tile = $(this).parent();
 
-      if (!expanded) {
-        tile.addClass('strips__strip--expanded');
-        // add delay to inner text
-        tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
-        stripClose.addClass('strip__close--show');
-        stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
-        expanded = true;
-      }
-    };
+    if (!expanded) {
+      tile.addClass('strips__strip--expanded');
+      // add delay to inner text
+      tileText.css('transition', 'all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)');
+      stripClose.addClass('strip__close--show');
+      stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
+      expanded = true;
+    }
+  };
 
   var close = function() {
     if (expanded) {
       tile.removeClass('strips__strip--expanded');
       // remove delay from inner text
       tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
+
       stripClose.removeClass('strip__close--show');
       stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
       expanded = false;
     }
-  }
+  };
+
+  var bindKey = function(e) {
+    if(e.keyCode == 27) {
+      close();
+    }
+  };
 
   var bindActions = function() {
     tileLink.on('click', open);
     stripClose.on('click', close);
+    $(document).on('keyup', bindKey);
   };
 
   var init = function() {
